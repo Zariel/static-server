326c4a4821a71616f28e9198a6ebd01a2c6dec5d
Chris Bannister <c.bannister@gmail.com>
2013-02-27 19:54:56 +0000

    LiveScript can not handle cluster in windows, so rewrite in javascript.

---
+++ static/server.js
@@ -0,0 +1,62 @@
+var http = require("http")
+var cluster = require("cluster")
+
+var proxy = require("http-proxy")
+var express = require("express")
+var ecstatic = require("ecstatic")
+
+var logger = function(req, res, next) {
+    console.log("(%d) => %s %s", cluster.worker.id, req.method, req.url)
+
+	return next()
+}
+
+var app = express()
+
+app.use(logger)
+app.use(ecstatic({
+    root: __dirname + "/public",
+    cache: 0,
+    gzip: true,
+    handleError: false,
+    autoIndex: true
+}))
+
+app.use(function(req, res, next) {
+	return res.status(200).sendfile(__dirname + "/public/index.html")
+})
+
+var opts = {
+	router: {
+		'localhost/api': 'localhost:8080/curiosity/api',
+		'.*': 'localhost:8082'
+	}
+}
+
+httpServer = http.createServer(app)
+proxyServer = proxy.createServer(logger, opts)
+
+if(cluster.isMaster) {
+	var numCpus = require("os").cpus().length
+
+	for(var i = 0; i < numCpus; i++) {
+		var opts = {
+			proxy: i % 2 == 0
+		}
+
+		cluster.fork(opts)
+	}
+
+	cluster.on('exit', function(worker, code, signal) {
+		return console.log('Worker ' + worker.id + ' died')
+	})
+} else {
+	var id = cluster.worker.id;
+	if (process.env.proxy === "true") {
+		console.log("[" + id + "] HTTP Proxy listening on port 80 ..")
+		proxyServer.listen(80)
+	} else {
+		console.log("[" + id + "] HTTP static server listening on port 8082 ..")
+		httpServer.listen(8082)
+	}
+}
